5991e96f9c2666f2d1b3c4f7961b8630192d376b
syoustra <steph.you07@gmail.com>
2018-09-10 13:51:23 -0400

    Add GetDataService interface, which ... sets up the call that will create the list of data?

---
+++ app/src/main/java/com/example/artsonian/retrofittutorial/network/GetDataService.java
@@ -0,0 +1,13 @@
+package com.example.artsonian.retrofittutorial.network;
+
+import com.example.artsonian.retrofittutorial.model.RetroPhoto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+}
